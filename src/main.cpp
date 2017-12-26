501c33058a4a1c6b9612435594826b064a9de55a
Kentaro Kawano <kentaro.kawano@gmail.com>
2017-12-27 00:18:10 +0900

    Implemented "Key A"

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,43 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
+#include "Keyboard.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#define INPUT_PIN 3
+
+bool prevStatus = LOW;
 
 void setup()
 {
-    // initialize LED digital pin as an output.
+    pinMode(INPUT_PIN, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
+
+    Keyboard.begin();
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN, HIGH);
+    bool status = digitalRead(INPUT_PIN);
+    if (prevStatus == status)
+    {
+        return;
+    }
 
-    // wait for a second
-    delay(1000);
+    prevStatus = status;
 
-    // turn the LED off by making the voltage LOW
-    digitalWrite(LED_BUILTIN, LOW);
+    switch (status)
+    {
+    case HIGH:
+        digitalWrite(LED_BUILTIN, HIGH);
+        Keyboard.press(0x61);
+        Serial.println("Press");
 
-    // wait for a second
-    delay(1000);
+        break;
+    case LOW:
+        digitalWrite(LED_BUILTIN, LOW);
+        Keyboard.release(0x61);
+        Serial.println("Release");
+
+        break;
+    }
 }
