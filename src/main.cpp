0662c45cc4e1354a57f20d70c151fd9b0a9dae66
Kentaro Kawano <kentaro.kawano@gmail.com>
2018-01-05 01:27:09 +0900

    Implemented KeyMap 2*2

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,39 @@
 #include "Arduino.h"
 #include "Keyboard.h"
 
-const int keyNum = 2;
+const int rowNum = 2;
+const int colNum = 2;
 
-const int inputPin[keyNum] = {3, 4};
-const byte keyMap[keyNum] = {0x61, 0x62};
+const int rowPin[rowNum] = {3, 4};
+const int colPin[colNum] = {5, 6};
+const byte keyMap[rowNum][colNum] = {
+    {0x61, 0x62},
+    {0x63, 0x64}};
 
-bool currentState[keyNum];
-bool beforeState[keyNum];
+bool currentState[rowNum][colNum];
+bool beforeState[rowNum][colNum];
 
 void setup()
 {
-  for (int i = 0; i < keyNum; i++)
+
+  for (int i = 0; i < rowNum; i++)
   {
-    pinMode(inputPin[i], INPUT);
-    currentState[i] = LOW;
-    beforeState[i] = LOW;
+    pinMode(rowPin[i], OUTPUT);
+  }
+
+  for (int i = 0; i < colNum; i++)
+  {
+    pinMode(colPin[i], INPUT);
+  }
+
+  for (int i = 0; i < rowNum; i++)
+  {
+    for (int j = 0; j < colNum; j++)
+    {
+      currentState[i][j] = LOW;
+      beforeState[i][j] = LOW;
+    }
+    digitalWrite(rowPin[i], LOW);
   }
 
   Serial.begin(9600);
@@ -24,27 +42,36 @@ void setup()
 
 void loop()
 {
-  for (int i = 0; i < keyNum; i++)
+  for (int i = 0; i < rowNum; i++)
   {
-    currentState[i] = digitalRead(inputPin[i]);
+    digitalWrite(rowPin[i], HIGH);
 
-    if (currentState[i] != beforeState[i])
+    for (int j = 0; j < colNum; j++)
     {
-      Serial.print("key");
-      Serial.print(i);
+      currentState[i][j] = digitalRead(colPin[j]);
 
-      if (currentState[i] == HIGH)
+      if (currentState[i][j] != beforeState[i][j])
       {
-        Serial.println("\tPush!");
-        Keyboard.press(keyMap[i]);
-      }
-      else
-      {
-        Serial.println("\tRelease!");
-        Keyboard.release(keyMap[i]);
-      }
 
-      beforeState[i] = currentState[i];
+        Serial.print("key(");
+        Serial.print(i);
+        Serial.print(",");
+        Serial.print(j);
+        Serial.print(")");
+
+        if (currentState[i][j] == HIGH)
+        {
+          Serial.println(" Push!");
+          Keyboard.press(keyMap[i][j]);
+        }
+        else
+        {
+          Serial.println(" Release!");
+          Keyboard.release(keyMap[i][j]);
+        }
+        beforeState[i][j] = currentState[i][j];
+      }
     }
+    digitalWrite(rowPin[i], LOW);
   }
 }
